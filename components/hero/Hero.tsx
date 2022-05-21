c6b877341a4cbd7dc6e2fd828be6a533b8c399b8
Nenad Brcic <neno@neno.ch>
2022-05-21 07:47:45 +0200

    Cleanup

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -18,7 +18,6 @@ export const Hero = () => {
             src='/images/Sunrise-v5-Ghashghai-meets-Bauhaus-SoFar-SoNear-Studio-for-Zollanvari.jpeg'
             className=''
             alt='Some alt text'
-            // placeholder='blur'
             layout='fill'
             objectFit='cover'
             objectPosition='right bottom'
@@ -41,24 +40,6 @@ export const Hero = () => {
           </p>
         </div>
       </div>
-      {/* <div className='container px-2 sm:px-4 lg:px-8 xl:px-8 2xl:px-0 mb-32 md:mb-44 relative'>
-        <div className='container aspect-square md:aspect-[2/1] relative'>
-          <Image
-            src='/images/Sunrise-v5-Ghashghai-meets-Bauhaus-SoFar-SoNear-Studio-for-Zollanvari.jpeg'
-            className=''
-            alt='Some alt text'
-            // placeholder='blur'
-            layout='fill'
-            objectFit='cover'
-            objectPosition='right bottom'
-          />
-          <div className='absolute w-[253px] md:w-[410px] xl:w-auto right-0 bottom-0 md:bottom-0 p-8 md:p-16 translate-x-2 sm:translate-x-4 lg:translate-x-8 xl:translate-x-[4vw] 2xl:translate-x-[3vw] translate-y-28 md:translate-y-16 text-white text-xl md:text-3xl font-title leading-relaxed bg-irmak-blue bg-gradient-to-br from-dark-blue via-irmak-blue to-light-blue opacity-90'>
-            Die Referenz in Bern für Teppiche &amp; Kelims.
-            <br />
-            Seit 1985.
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 };
