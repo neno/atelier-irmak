c40ee1c1125c82dbf509c9503a334a6ef54130e0
Josip Anic <josipanic1985@gmail.com>
2023-01-19 20:22:39 +0100

    styling slider arrows

--- src/ui/slider/Slider.tsx
+++ src/ui/slider/Slider.tsx
@@ -16,24 +16,25 @@ interface SliderProps {
 export const Slider: FC<SliderProps> = ({ items, className }) => { 
   return (
   <Splide aria-label="My Favorite Images"
-      options={ {
-        height   : '30rem',
-        gap      : '0.625rem',
-        autoWidth: true,
-      } }
-      className={clsxm(className, styles.slider)}
-    >
-      {items.map(({ url, description, width, height }) => (
-        <SplideSlide key={url}>
-          <NextImage
-            src={url}
-            alt={description}
-            width={width}
-            height={height}
-            className='w-full h-full object-cover object-bottom'
-            sizes='(min-width: 640px) 30vw, (min-width: 1280px) 500px, 100vw'
-          />
-        </SplideSlide>
-      ))}
+    options={ {
+      height   : '30rem',
+      gap      : '0.625rem',
+      arrowPath: 'm12.77,35.53l-1.06-1.06,14.47-14.47L11.7,5.53l1.06-1.06,15.53,15.53-15.53,15.53Z',
+      autoWidth: true,
+    } }
+    className={clsxm(className, styles.slider)}
+  >
+    {items.map(({ url, description, width, height }) => (
+      <SplideSlide key={url}>
+        <NextImage
+          src={url}
+          alt={description}
+          width={width}
+          height={height}
+          className='w-full h-full object-cover object-bottom'
+          sizes='(min-width: 640px) 30vw, (min-width: 1280px) 500px, 100vw'
+        />
+      </SplideSlide>
+    ))}
   </Splide>
 )}
