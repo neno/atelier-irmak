ac64b06c0439a1f2cd8a17f54e3f64f15f7c7c0b
Josip Anic <josipanic1985@gmail.com>
2023-02-07 10:05:37 +0100

    wip

--- src/ui/Nav.tsx
+++ src/ui/Nav.tsx
@@ -12,9 +12,14 @@ interface NavProps {
 
 export const Nav: FC<NavProps> = ({ items }) => {
   const [open, setOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setOpen(!open);
+  };
+
   return (
     <nav>
-      <button type="button" className={clsxm("inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white sm:hidden")}>
+      <button onClick={toggleMenu} type="button" className={clsxm("inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white sm:hidden")}>
         <span className="sr-only">Open main menu</span>
         {open ? (
           <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
@@ -22,10 +27,10 @@ export const Nav: FC<NavProps> = ({ items }) => {
           <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
         )}
       </button>
-      <ul className={clsxm('list-none font-medium hidden sm:flex')}>
+      <ul className={clsxm('fixed inset-0 top-[60px] z-10 overflow-y-auto bg-white list-none font-medium sm:flex sm:static', {'hidden' : !open})}>
         {items.map(({ title, page, externalUrl }) => (
-          <li key={title}>
-            <Link href={`${page?.slug ?? externalUrl}`} className='py-4 pl-8'>
+          <li key={title} className={clsxm('mt-4 sm:mt-0')}>
+            <Link href={`${page?.slug ?? externalUrl}`} className='text-xl sm:text-base sm:py-4 pl-8'>
               {title}
             </Link>
           </li>
