f0149a53f6b275be5292b19d1b2cf90afe95145e
Josip Anic <josipanic1985@gmail.com>
2023-02-10 10:27:27 +0100

    switch to mobile nav from 1023

--- src/ui/Nav.tsx
+++ src/ui/Nav.tsx
@@ -23,7 +23,7 @@ export const Nav: FC<NavProps> = ({ items }) => {
 
   return (
     <nav>
-      <button onClick={toggleMenu} type="button" className={clsxm("inline-flex items-center justify-center rounded-md -mr-[0.5rem] p-2 text-gray-400 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white sm:hidden")}>
+      <button onClick={toggleMenu} type="button" className={clsxm("inline-flex items-center justify-center rounded-md -mr-[0.5rem] p-2 text-gray-400 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white lg:hidden")}>
         <span className="sr-only">Open main menu</span>
         {open ? (
           <XMarkIcon className="block h-8 w-8" aria-hidden="true" />
@@ -31,10 +31,10 @@ export const Nav: FC<NavProps> = ({ items }) => {
           <Bars3Icon className="block h-8 w-8" aria-hidden="true" />
         )}
       </button>
-      <ul className={clsxm('fixed inset-0 top-[60px] z-10 overflow-y-auto bg-white list-none font-medium sm:flex sm:static', {'hidden' : !open})}>
+      <ul className={clsxm('fixed inset-0 top-[5.4375rem] sm:top-[7.4375rem] md:top-[8.5625rem] z-10 overflow-y-auto bg-white list-none font-medium lg:flex lg:static', {'hidden' : !open})}>
         {items.map(({ title, page, externalUrl }) => (
-          <li key={title} className={clsxm('mt-4 px-4 sm:mt-0')}>
-            <Link onClick={closeMenu} href={`${page?.slug ?? externalUrl}`} className='text-xl block sm:text-base py-2 border-b-2 border-transparent sm:hover:text-primary-dark sm:hover:border-primary-dark'>
+          <li key={title} className={clsxm('mt-4 px-4 lg:mt-0')}>
+            <Link onClick={closeMenu} href={`${page?.slug ?? externalUrl}`} className='text-xl block lg:text-base py-2 border-b-2 border-transparent lg:hover:text-primary-dark lg:hover:border-primary-dark'>
               {title}
             </Link>
           </li>
