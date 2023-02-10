4a4832daf66709bb51872b840e249166a8f1c474
Josip Anic <josipanic1985@gmail.com>
2023-02-10 09:20:11 +0100

    increase size of logo and menu button on mobile

--- src/ui/Nav.tsx
+++ src/ui/Nav.tsx
@@ -26,9 +26,9 @@ export const Nav: FC<NavProps> = ({ items }) => {
       <button onClick={toggleMenu} type="button" className={clsxm("inline-flex items-center justify-center rounded-md -mr-[0.5rem] p-2 text-gray-400 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white sm:hidden")}>
         <span className="sr-only">Open main menu</span>
         {open ? (
-          <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+          <XMarkIcon className="block h-8 w-8" aria-hidden="true" />
         ) : (
-          <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+          <Bars3Icon className="block h-8 w-8" aria-hidden="true" />
         )}
       </button>
       <ul className={clsxm('fixed inset-0 top-[60px] z-10 overflow-y-auto bg-white list-none font-medium sm:flex sm:static', {'hidden' : !open})}>
