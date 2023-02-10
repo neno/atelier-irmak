fd55947a7ae8824744551b8e81cbec5ecdbe747a
Josip Anic <josipanic1985@gmail.com>
2023-02-10 09:53:25 +0100

    set active state

--- src/ui/Nav.tsx
+++ src/ui/Nav.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import {FC, useState} from 'react';
 import {Bars3Icon, XMarkIcon} from "@heroicons/react/24/outline";
 import clsxm from "@/lib/clsxm";
+import {usePathname} from 'next/navigation';
 
 interface NavProps {
   items: ILink[];
@@ -12,6 +13,7 @@ interface NavProps {
 
 export const Nav: FC<NavProps> = ({ items }) => {
   const [open, setOpen] = useState(false);
+  const pathname = usePathname();
 
   const toggleMenu = () => {
     setOpen(!open);
@@ -33,8 +35,8 @@ export const Nav: FC<NavProps> = ({ items }) => {
       </button>
       <ul className={clsxm('fixed inset-0 top-[60px] z-10 overflow-y-auto bg-white list-none font-medium sm:flex sm:static', {'hidden' : !open})}>
         {items.map(({ title, page, externalUrl }) => (
-          <li key={title} className={clsxm('mt-4 px-4 sm:mt-0')}>
-            <Link onClick={closeMenu} href={`${page?.slug ?? externalUrl}`} className='text-xl block sm:text-base py-2 border-b-2 border-transparent sm:hover:text-primary-dark sm:hover:border-primary-dark'>
+          <li key={title} className={clsxm('sm:px-4 sm:mt-0')}>
+            <Link onClick={closeMenu} href={`${page?.slug ?? externalUrl}`} className={clsxm('text-xl block sm:text-base py-4 px-4 border-b-2 border-transparent sm:py-2 sm:px-0 sm:hover:text-primary-dark sm:hover:border-primary-dark', pathname?.includes(page?.slug as string) && 'bg-primary text-white sm:bg-transparent sm:text-primary-light')}>
               {title}
             </Link>
           </li>
