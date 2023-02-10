101ac36233e93395cfb1768652a9aeee647d5b22
Josip Anic <josipanic1985@gmail.com>
2023-02-10 14:57:24 +0100

    fix scrolling

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -7,7 +7,7 @@ interface HeaderProps {
 }
 export const Header: FC<HeaderProps> = ({ children }) => {
   return (
-    <Container className='fixed top-0 left-0 z-10 p-4 text-primary-dark bg-white flex items-center justify-between md:max-w-full lg:p-8 lg:relative'>
+    <Container className='fixed top-0 left-0 z-20 p-4 text-primary-dark bg-white flex items-center justify-between md:max-w-full lg:p-8 lg:relative'>
       <Link href='/'>
         <Logo />
       </Link>
--- src/ui/Nav.tsx
+++ src/ui/Nav.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 import {FC, useState} from 'react';
 import {Bars3Icon, XMarkIcon} from "@heroicons/react/24/outline";
 import clsxm from "@/lib/clsxm";
-import {usePathname} from 'next/navigation';
+import {usePathname, useRouter} from 'next/navigation';
 
 interface NavProps {
   items: ILink[];
@@ -14,13 +14,19 @@ interface NavProps {
 export const Nav: FC<NavProps> = ({ items }) => {
   const [open, setOpen] = useState(false);
   const pathname = usePathname();
+  const router = useRouter();
 
   const toggleMenu = () => {
     setOpen(!open);
   };
 
-  const closeMenu = () => {
+  const onLinkClick = (e: React.MouseEvent<HTMLAnchorElement>, url: string | undefined) => {
+    e.preventDefault();
     setOpen(false);
+    if (url) {
+      router.push(url)
+      window.scroll(0, 0)
+    }
   };
 
   return (
@@ -33,12 +39,12 @@ export const Nav: FC<NavProps> = ({ items }) => {
           <Bars3Icon className="block h-8 w-8" aria-hidden="true" />
         )}
       </button>
-      <ul className={clsxm('fixed inset-0 top-[5.4375rem] md:top-[6.5625rem] lg:top-[8.5625rem] z-10 overflow-y-auto bg-white list-none font-medium lg:flex lg:static', {'hidden' : !open})}>
+      <ul className={clsxm('fixed inset-0 top-[5.4375rem] md:top-[6.5625rem] lg:top-[8.5625rem] overflow-y-auto bg-white list-none font-medium lg:flex lg:static', {'hidden' : !open})}>
         {items.map(({ title, page, externalUrl }) => (
           <li key={title} className={clsxm('mt-4 lg:px-4 lg:mt-0')}>
-            <Link onClick={closeMenu} href={`${page?.slug ?? externalUrl}`} className={clsxm('text-xl block lg:text-base p-4 lg-px-0 lg:py-2 border-b-2 border-transparent lg:hover:text-primary-dark lg:hover:border-primary-dark', pathname?.includes(page?.slug as string) && 'bg-primary text-white lg:bg-transparent lg:text-primary-light')}>
+            <a onClick={(e) => onLinkClick(e, page?.slug ?? externalUrl)} href={`${page?.slug ?? externalUrl}`} className={clsxm('text-xl block lg:text-base p-4 lg-px-0 lg:py-2 border-b-2 border-transparent lg:hover:text-primary-dark lg:hover:border-primary-dark', pathname?.includes(page?.slug as string) && 'bg-primary text-white lg:bg-transparent lg:text-primary-light')}>
               {title}
-            </Link>
+            </a>
           </li>
         ))}
       </ul>
