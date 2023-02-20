fcbe644cbbfbc6bf7280bb9f18d500c769e6ec57
Nenad Brcic <neno@neno.ch>
2023-02-20 10:34:59 +0100

    Cleanup

--- src/ui/flying-carpet/flying-carpet-header/FlyingCarpetHeader.tsx
+++ src/ui/flying-carpet/flying-carpet-header/FlyingCarpetHeader.tsx
@@ -4,24 +4,7 @@ import Link from 'next/link';
 import { FC } from 'react';
 import { RichText } from '../../rich-text/RichText';
 import { ArrowRightCircleIcon } from '@heroicons/react/24/outline';
-
-<svg
-  xmlns='http://www.w3.org/2000/svg'
-  fill='none'
-  viewBox='0 0 24 24'
-  strokeWidth={1.5}
-  stroke='currentColor'
-  className='w-6 h-6'
->
-  <path
-    strokeLinecap='round'
-    strokeLinejoin='round'
-    d='M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
-  />
-</svg>;
-
 import styles from './FlyingCarpetHeader.module.css';
-import { useRouter } from 'next/router';
 
 export const FlyingCarpetHeader: FC<
   Pick<IFlyingCarpet, 'template' | 'text' | 'link' | 'backgroundColor'>
