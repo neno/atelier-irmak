0e0baa33956adc4f44214a9227f1a3858cc04b37
Josip Anic <josipanic1985@gmail.com>
2023-07-10 16:46:17 +0200

    add next-sitemap with basic paths

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,10 +2,5 @@
 module.exports = {
     siteUrl: process.env.SITE_URL,
     generateRobotsTxt: true,
-    exclude: ['/server-sitemap.xml'],
-    robotsTxtOptions: {
-        additionalSitemaps: [
-            process.env.SITE_URL + '/server-sitemap.xml',
-        ],
-    },
+    generateIndexSitemap: false,
 }
