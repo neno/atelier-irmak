94f447d15483eb33c0addff8d54bc612b9200c3d
Josip Anić <josipanic1985@gmail.com>
2023-08-22 09:35:23 +0200

    Kategorien (#29)
    * remove kategorie section from carpet page
    
    * remove "Kategorien" section from carpet page
    
    * remove "Kategorien" section from carpet page

--- src/app/kollektion/[slug]/page.tsx
+++ src/app/kollektion/[slug]/page.tsx
@@ -6,9 +6,6 @@ import { Container } from '@/ui/Container';
 import { ContainerVertical } from '@/ui/ContainerVertical';
 import { DefList } from '@/ui/DefList';
 import { SliderWithModal } from '@/ui/SliderWithModal';
-import { Palette } from '@/ui/palette/Palette';
-import { Colors } from '@/ui/colors/Colors';
-import { RugCategorizations } from '@/ui/rug-categorizations/RugCategorizations';
 import clsxm from '@/lib/clsxm';
 import { Metadata } from 'next';
 import {createMetadata} from "@/lib/helpers";
@@ -29,22 +26,17 @@ async function RugPage({ params }: { params: { slug: string } }) {
     excerpt,
     description,
     name,
-    type,
     origin,
-    country,
     length,
     width,
-    age,
     dating,
     featuredImage,
-    palette,
-    colors,
     galleryCollection: { items: galleryItems },
   } = rug;
   const size = `${length} x ${width} cm`;
 
   return (
-    <ContainerVertical className=''>
+    <ContainerVertical className='pb-16'>
       <DetailHeader title={title} subtitle={subtitle} image={featuredImage} />
       <Container className='lg:my-16'>
         <LeadText leadText={excerpt} />
@@ -82,24 +74,6 @@ async function RugPage({ params }: { params: { slug: string } }) {
       <Container>
         <SliderWithModal galleryItems={galleryItems} />
       </Container>
-      <div className='w-full bg-gray'>
-        <Container className='py-8 sm:py-16'>
-          <ContainerVertical tag='aside' className='gap-4 md:gap-8'>
-            <h3>Kategorien</h3>
-            <div className='flex flex-col gap-4 sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 lg:gap-16'>
-              <Colors colors={colors} />
-              <Palette palette={palette} />
-              <RugCategorizations
-                categorizations={{
-                  type,
-                  age: age.name,
-                  country: country.name,
-                }}
-              />
-            </div>
-          </ContainerVertical>
-        </Container>
-      </div>
     </ContainerVertical>
   );
 }
