66690b37b9369eed331d7ad209d584c64bdaa464
Josip Anić <josipanic1985@gmail.com>
2023-09-07 16:32:37 +0200

    close navigation on link click (#36)

--- src/ui/nav/NavLink.tsx
+++ src/ui/nav/NavLink.tsx
@@ -4,6 +4,8 @@ import clsxm from '@/lib/clsxm';
 import Link from 'next/link';
 import { useSelectedLayoutSegment } from 'next/navigation';
 import { FC } from 'react';
+import {useAtom} from "jotai/index";
+import {navAtom} from "@/store/nav-atom";
 
 interface NavLinkProps {
   href: string;
@@ -11,12 +13,14 @@ interface NavLinkProps {
 }
 
 export const NavLink: FC<NavLinkProps> = ({ href, children }) => {
+  const [, setOpen] = useAtom(navAtom);
   const segment = useSelectedLayoutSegment();
   const isActive = href === `/${segment}`;
   return (
     <Link
       href={href}
       scroll={true}
+      onClick={() => setOpen(false)}
       className={clsxm(
         'text-xl block lg:text-base p-4 lg:px-0 lg:py-2 border-b-2 border-transparent lg:hover:text-primary-light',
         {
