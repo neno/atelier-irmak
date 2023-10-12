1f8de939a2dd0bf90026ea07b3d441a8385135c7
Nenad Brcic <neno@neno.ch>
2023-10-12 09:00:05 +0200

    Fix prettier

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,21 +1,23 @@
-import {ContainerVertical} from "@/ui/ContainerVertical";
-import {Container} from "@/ui/Container";
-import {Button} from "@/ui/Button";
+import { ContainerVertical } from '@/ui/ContainerVertical';
+import { Container } from '@/ui/Container';
+import { Button } from '@/ui/Button';
 
 export default function NotFound() {
   return (
     <ContainerVertical className='py-16'>
-      <Container className="text-center max-w-3xl">
+      <Container className='text-center max-w-3xl'>
         <h1>Seite nicht gefunden</h1>
-        <p className="mt-8">Diese Seite konnte nicht gefunden werden.</p>
-        <p className="mt-4">
+        <p className='mt-8'>Diese Seite konnte nicht gefunden werden.</p>
+        <p className='mt-4'>
           Gründe dafür könnten sein, dass Sie eine falsche oder veraltete URL
-          aufgerufen haben. Wir bitten Sie, diese noch einmal zu überprüfen.
-          Es ist auch möglich, dass wir die betreffende Seite verschoben,
+          aufgerufen haben. Wir bitten Sie, diese noch einmal zu überprüfen. Es
+          ist auch möglich, dass wir die betreffende Seite verschoben,
           archiviert oder umbenannt haben.
         </p>
-        <Button className="inline-block py-2 font-medium mt-12" path="/">Zur Startseite</Button>
+        <Button className='inline-block py-2 font-medium mt-12' path='/'>
+          Zur Startseite
+        </Button>
       </Container>
     </ContainerVertical>
-  )
+  );
 }
