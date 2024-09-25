37fd737c277b086c36aa59f8fe6c374261cfc5a9
Nenad Brcic <neno@neno.ch>
2024-09-25 08:35:40 +0200

    Cleanup

--- src/ui/hero/Hero.tsx
+++ src/ui/hero/Hero.tsx
@@ -13,35 +13,4 @@ export const Hero: FC<IHero> = ({ type, image, title, sliderCollection }) => {
   }
 
   throw new Error(`Hero type ${type} not found`);
-  // return (
-  //   <Container
-  //     className={
-  //       clsxm('relative grid grid-cols-12 pb-16', 'aspect-square aspect-[2/1]')
-  //       // 'relative grid grid-cols-12 pb-16'
-  //       // 'aspect-square aspect-[2/1]'
-  //     }
-  //   >
-  //     <div className={clsxm(styles.imageWrapper)}>
-  //       <NextImage
-  //         src={image.url}
-  //         width={image.width}
-  //         height={image.height}
-  //         alt={image.description}
-  //         priority={true}
-  //         className='aspect-square md:aspect-[2/1] object-cover object-center'
-  //         sizes='(min-width: 90em) 90rem, 100vw'
-  //       />
-  //     </div>
-  //     <div className={clsxm(styles.textWrapper)}>
-  //       <h1
-  //         className={clsxm(
-  //           styles.title,
-  //           'text-white text-xl lg:text-3xl font-title leading-relaxed lg:leading-tight'
-  //         )}
-  //       >
-  //         {title}
-  //       </h1>
-  //     </div>
-  //   </Container>
-  // );
 };
