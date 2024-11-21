54e1d3623d4517a0a941a619cdc7f95404d2a355
Nenad Brcic <neno@neno.ch>
2024-11-21 16:01:28 +0100

    Make link optional

--- src/ui/flying-carpet/flying-carpet-header/FlyingCarpetHeader.tsx
+++ src/ui/flying-carpet/flying-carpet-header/FlyingCarpetHeader.tsx
@@ -9,11 +9,32 @@ import styles from './FlyingCarpetHeader.module.css';
 export const FlyingCarpetHeader: FC<
   Pick<IFlyingCarpet, 'template' | 'text' | 'link' | 'backgroundColor'>
 > = ({ template, link, text, backgroundColor }) => {
+  
+
+  if (!link) {
+    return (
+      <header className={clsxm(styles[`txt--${template}`])}>
+        <div
+          
+          className={clsxm(styles.link, styles[`bg--${backgroundColor}`])}
+        >
+          <RichText
+            content={text}
+            className='flex flex-col flex-1 justify-between w-full h-full '
+          />
+          <div className='flex justify-end'>
+            <ArrowRightCircleIcon className='block h-8 w-8 text-white' />
+          </div>
+        </div>
+      </header>
+    );
+  }
+
   const path = `/referenzen/${link.slug}`; // TODO: Make this dynamic
   return (
     <header className={clsxm(styles[`txt--${template}`])}>
       <Link
-        href={path}
+        href={path ?? ''}
         className={clsxm(styles.link, styles[`bg--${backgroundColor}`])}
       >
         <RichText
