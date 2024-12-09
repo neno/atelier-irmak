755c3c5e6ed9535cb1fdf23162d20ec78f71e2de
Nenad Brcic <neno@neno.ch>
2024-12-09 17:39:35 +0100

    Fix it!

--- src/ui/Features.tsx
+++ src/ui/Features.tsx
@@ -15,36 +15,41 @@ export const Features: FC<FeaturesProps> = ({
   items,
   background = "none",
   columns,
-}) => (
-  <div
-    className={clsxm("features", {
-      "w-full xl:mt-24": background !== "none",
-      "bg-gray": background === "light",
-      "bg-primary": background === "dark",
-    })}
-  >
-    <Container className={clsxm("py-8 sm:py-16")}>
-      <div
-        className={clsxm(
-          "grid mx-auto md:max-w-4xl lg:max-w-full gap-4 lg:gap-8 xl:gap-12 2xl:gap-16 xl:grid-cols-3",
-          `xl:grid-cols-${columns ?? 3}`
-        )}
-      >
-        {items.map((item) => (
-          <section className="relative" key={item.sys.id}>
-            {isFeature(item) ? (
-              <Feature
-                media={item.media}
-                title={item.title}
-                text={item.text}
-                page={item.page}
-              />
-            ) : (
-              <RichText content={item.richText} />
-            )}
-          </section>
-        ))}
-      </div>
-    </Container>
-  </div>
-);
+}) => {
+  console.log("columns", columns);
+
+  const gridCols = columns === 2 ? "xl:grid-cols-2" : "xl:grid-cols-3";
+  return (
+    <div
+      className={clsxm("features", {
+        "w-full xl:mt-24": background !== "none",
+        "bg-gray": background === "light",
+        "bg-primary": background === "dark",
+      })}
+    >
+      <Container className={clsxm("py-8 sm:py-16")}>
+        <div
+          className={clsxm(
+            "grid mx-auto md:max-w-4xl lg:max-w-full gap-4 lg:gap-8 xl:gap-12 2xl:gap-16",
+            gridCols
+          )}
+        >
+          {items.map((item) => (
+            <section className="relative" key={item.sys.id}>
+              {isFeature(item) ? (
+                <Feature
+                  media={item.media}
+                  title={item.title}
+                  text={item.text}
+                  page={item.page}
+                />
+              ) : (
+                <RichText content={item.richText} />
+              )}
+            </section>
+          ))}
+        </div>
+      </Container>
+    </div>
+  );
+};
